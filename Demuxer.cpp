cb83f1bdc86b5df7232c2c3f5c9668887996b239
isxxguo <isxxguo@pptv.com>
2013-02-01 03:03:22 +0000

    /ppbox/branches/1.2.0 /src/plugins 增加 winrt/PpboxRT
    git-svn-id: http://svn01/repositories/r01/ppbox/branches/1.2.0@6270 b3a1389e-2159-5c43-9038-463bf877d0d9

---
+++ Demuxer.cpp
@@ -0,0 +1,144 @@
+﻿// Demuxer.cpp
+
+#include "Demuxer.h"
+
+#include <plugins/ppbox/ppbox.h>
+
+#include <windows.h>
+
+using namespace PpboxRT;
+using namespace Platform;
+
+namespace PpboxRT
+{
+	class Access
+	{
+	public:
+		static Stream ^ new_stream(
+			PPBOX_StreamInfoEx & stream)
+		{
+			return ref new Stream(stream);
+		}
+
+		static Media ^ new_media(
+			uint64 duration, 
+			Array<Stream ^> ^ streams)
+		{
+			return ref new Media(duration, streams);
+		}
+
+		static Sample ^ new_sample(
+			PPBOX_SampleEx2 & sample)
+		{
+			return ref new Sample(sample);
+		}
+	};
+}
+
+Stream::Stream(
+	PPBOX_StreamInfoEx & info)
+{
+	type_ = (StreamType)info.type;
+	sub_type_ = (StreamSubType)info.sub_type;
+	if (type_ == StreamType::video) {
+		video_.width = info.video_format.width;
+		video_.height = info.video_format.height;
+		video_.frame_rate = info.video_format.frame_rate;
+	} else {
+		audio_.channel_count = info.audio_format.channel_count;
+		audio_.sample_rate = info.audio_format.sample_rate;
+		audio_.sample_size = info.audio_format.sample_size;
+	}
+	codec_data_ = ref new Platform::Array<uint8>((uint8 *)info.format_buffer, info.format_size);
+}
+
+Media::Media(
+	uint64 duration, 
+	Array<Stream ^> ^ streams)
+	: duration_(duration)
+	, streams_(streams)
+{
+}
+
+Sample::Sample(
+	PPBOX_SampleEx2 & sample)
+{
+	index_ = sample.stream_index;
+	flag_ = 0;
+	if (sample.is_sync) {
+		flag_ |= sync;
+	}
+	if (sample.is_discontinuity) {
+		flag_ |= discontinue;
+	}
+	time_ = sample.start_time;
+	data_ = ref new Platform::Array<uint8>((uint8 *)sample.buffer, sample.buffer_length);
+}
+
+Demuxer::Demuxer()
+{
+}
+
+void Demuxer::async_open(
+	String ^ playlink, 
+	String ^ config, 
+	Callback ^ callback)
+{
+	char playlinkc[MAX_PATH];
+	char configc[MAX_PATH];
+
+	WideCharToMultiByte(CP_ACP, 0, playlink->Data(), -1, playlinkc, MAX_PATH, 0, 0);
+	WideCharToMultiByte(CP_ACP, 0, config->Data(), -1, configc, MAX_PATH, 0, 0);
+
+	self_ref_ = this;
+	PPBOX_AsyncOpenEx(playlinkc, configc, &self_ref_, s_call_back);
+}
+
+Error Demuxer::get_media(
+	Media ^* media)
+{
+	PP_uint64 d = PPBOX_GetDuration();
+	PP_uint32 n = PPBOX_GetStreamCount();
+	Array<Stream ^> ^ streams = ref new Platform::Array<Stream ^>(n);
+	for (PP_uint32 i = 0; i < n; ++i) {
+		PPBOX_StreamInfoEx info;
+		PPBOX_GetStreamInfoEx(i, &info);
+		Stream ^ s = Access::new_stream(info);
+		streams->set(i, s);
+	}
+	*media = Access::new_media(d * 10000, streams);
+	return Error::success;
+}
+
+Error Demuxer::seek(
+	uint64 time)
+{
+	return (Error)PPBOX_Seek((PP_uint32)(time / 10000));
+}
+
+Error Demuxer::get_sample(
+	Sample ^* sample)
+{
+	PPBOX_SampleEx2 s;
+	PP_err ec = PPBOX_ReadSampleEx2(&s);
+	if (ec == 0) {
+		*sample = Access::new_sample(s);
+	}
+	return (Error)ec;
+}
+
+void Demuxer::close()
+{
+	PPBOX_Close();
+}
+
+void Demuxer::s_call_back(
+	void * ctx, 
+	long ec)
+{
+	Demuxer ^ inst = *(Demuxer ^ *)ctx;
+	inst->self_ref_ = nullptr;
+
+	inst->callback_->Invoke((Error)ec);
+	inst->callback_ = nullptr;
+}
---
+++ Demuxer.h
@@ -0,0 +1,173 @@
+﻿#pragma once
+
+using namespace Platform;
+using namespace Windows::Foundation::Collections;
+
+struct PPBOX_StreamInfoEx;
+struct PPBOX_SampleEx2;
+
+namespace PpboxRT
+{
+
+	public enum class Error: int
+	{
+		success = 0, 
+		not_start, 
+		already_start, 
+		not_open, 
+		already_open, 
+		operation_canceled, 
+		would_block, 
+		stream_end, 
+		logic_error, 
+		network_error, 
+		demux_error, 
+		certify_error, 
+		other_error = 1024, 
+	};
+
+	public enum class StreamType: int
+    {
+        video = 1, 
+        audio = 2, 
+    };
+
+	public enum class StreamSubType: int
+    {
+        video_avc = 1, 
+        audio_aac = 2, 
+        audio_mp3 = 3, 
+        audio_wma = 4,
+    };
+
+    public value struct VideoInfo
+    {
+        uint32 width;
+		uint32 height;
+        uint32 frame_rate;
+    };
+
+    public value struct AudioInfo
+    {
+        uint32 channel_count;
+        uint32 sample_size;
+        uint32 sample_rate;
+    };
+
+	class Access;
+
+	public delegate void Callback(Error);
+
+	public ref class Stream sealed
+	{
+	public:
+		property StreamType type { StreamType get() { return type_; } }
+
+		property StreamSubType sub_type { StreamSubType get() { return sub_type_; } }
+
+		property VideoInfo video { VideoInfo get() { return video_; } }
+
+		property AudioInfo audio { AudioInfo get() { return audio_; } }
+
+		property Array<uint8> ^ codec_data { Array<uint8> ^ get() { return codec_data_; } }
+
+	private:
+		Stream(
+			PPBOX_StreamInfoEx & stream);
+
+		friend class Access;
+
+	private:
+		StreamType type_;
+		StreamSubType sub_type_;
+
+		VideoInfo video_;
+		AudioInfo audio_;
+
+		Array<uint8> ^ codec_data_;
+	};
+
+	public ref class Media sealed
+	{
+	public:
+		property uint64 duration { uint64 get() { return duration_; }}
+
+		property Array<Stream ^> ^ streams { Array<Stream ^> ^ get() { return streams_; }}
+
+	private:
+		Media(
+			uint64 duration, 
+			Array<Stream ^> ^ streams);
+
+		friend class Access;
+
+	private:
+		uint64 duration_;
+		Array<Stream ^> ^ streams_;
+	};
+
+	public ref class Sample sealed
+	{
+	public:
+		property uint32 index { uint32 get() { return index_; } }
+
+		property uint64 time { uint64 get() { return time_; } }
+
+		property bool is_sync { bool get() { return (flag_ & sync) != 0; } }
+
+		property bool is_discontinue { bool get() { return (flag_ & discontinue) != 0; } }
+
+		property Array<uint8> ^ data { Array<uint8> ^ get() { return data_; } }
+
+	private:
+		Sample(
+			PPBOX_SampleEx2 & sample);
+
+		friend class Access;
+
+	private:
+		enum
+		{
+			sync = 1, 
+			discontinue = 2, 
+		};
+		
+        uint32 index_;
+        uint32 flag_;
+        uint64 time_;
+		Array<uint8> ^ data_;
+	};
+
+    public ref class Demuxer sealed
+    {
+    public:
+        Demuxer();
+
+    public:
+		void async_open(
+			String ^ playlink, 
+			String ^ config, 
+			Callback ^ callback);
+
+		Error get_media(
+			Media ^* media);
+
+		Error seek(
+			uint64 time);
+
+		Error get_sample(
+			Sample ^* sample);
+
+		void close();
+
+	private:
+		static void s_call_back(
+			void * ctx, 
+			long ec);
+
+	private:
+		Demuxer ^ self_ref_;
+		Callback ^ callback_;
+    };
+
+}
